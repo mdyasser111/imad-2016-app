9172a0db4e3c887474fb8fbf3b80f72180c23022
mdyasser111 <mdyasser111@gmail.com>
2016-11-11 16:06:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg'),pool;
+var crypto = require('crypto');
+var bodyparser = require('body-parser');
+var session = require('express-session');
+
+var config = {
+    user     : 'mdyasser111',
+    database : 'mdyasser111',
+    host     : 'db.imad.hasura-app.io',
+    port     : '5432'
+    password : process.    .DB-PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyparser.json());
+
+app.use(session((
+    secret : 'someRandomSecret value',
+    cookie : {manAge : 200*60*60*24*34}
+    }));
+    
+    
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
