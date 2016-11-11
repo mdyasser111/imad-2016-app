2e3c67a4790094ce04bbcc3ef9dc7310d977175a
mdyasser111 <mdyasser111@gmail.com>
2016-11-11 15:50:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,20 @@
 
 //submit username/password to login
-var submit = document.getElementByid("submit_btm");
-submit.Onclick = function(){
+var submit = document.getElementById("submit_btm");
+submit.Onclick = function () {
     
     //create a request object
     var request = new XMLHttpRequest();
     
     //create a response and store it in a variable
     request.Onreadystatechane = function () {
-        if (request.readystate==XMLHttpRequest.Done) {
+        if (request.readystate === XMLHttpRequest.Done) {
             //take some action
-            if (request.status===200) {
+            if (request.status === 200) {
                 alert('logged in successfuly');
-            } else if (request status ===423) {
+            } else if (request. status === 423) {
                 alert('username/password is incorrect');
-            } else if  (request status===500) {
+            } else if  (request. status === 500) {
                 alert('some thing want wrong gateway 504 ');
             }
         }
@@ -28,6 +28,6 @@ submit.Onclick = function(){
     console.log(password);
     request.open('post','http:// mdyasser111.imad.hasura-app.io/',true);
     request.setRequestHeader('content.Type','application/json');
-    request.send(JSON.strinfigy((username: username,password: password)));
+    request.send(JSON.stringify({username: username,password: password}));
 
 };
